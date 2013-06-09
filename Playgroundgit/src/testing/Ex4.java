6fc918e8074e368dfd090827f21b5b76338fee61
Peter Thiemann <peter.thiemann@gmail.com>
2013-06-09 22:53:44 +0200

    debugging example

---
+++ Playgroundgit/src/testing/Ex4.java
@@ -0,0 +1,20 @@
+package testing;
+
+public class Ex4 {
+	public static int search( int[] array, int target ) {
+		int low = 0; 
+		int high = array.length; 
+		int mid;
+		while ( low <= high ) {
+			mid = (low + high)/2; 
+			if ( target < array[ mid ] ) {
+				high = mid - 1;
+			} else if ( target > array[ mid ] ) {
+				low = mid + 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+}
